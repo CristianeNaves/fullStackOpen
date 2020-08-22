42e691e794dc5050b7bb56ed91048d01c51ed3e2
Cristiane Naves <cristianenavescardoso09@gmail.com>
2020-08-21 21:40:09 -0300

    phonebook part6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Person = ({name, number}) => {
   return (
@@ -72,12 +73,7 @@ const Filter = ({filterName, setFilterName}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [ filterName, setFilterName ] = useState('')
 
@@ -86,6 +82,12 @@ const App = () => {
     let exp = filterName.toLowerCase();
     return persons.filter(person => person.name.toLowerCase().includes(exp))
   }
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response =>{
+      setPersons(response.data)
+    })
+  }, []);
 
   return (
     <div>
